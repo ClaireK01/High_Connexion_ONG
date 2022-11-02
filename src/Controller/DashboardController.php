a09ff8c7b72d9ee0111114e3cdd2818e6c79bf2c
ClaireK01 <ckalsto01@gmail.com>
2022-11-02 16:08:19 +0100

    ajout requete api piechart

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -50,16 +50,32 @@ class DashboardController extends AbstractController
     #[Route('/api_piechart', name: 'app_api_piechart', options:['expose'=>true])]
     public function api_pieChart(): JsonResponse
     {
+        //Connexion db
         $dbc = mysqli_connect('localhost', 'root', "", "high_connexion");
+
+        //Instanciation tableau de données
         $datas = [];
+
+        // Requête des 10 premiers département
         $departement = mysqli_query($dbc, '
-        SELECT COUNT(dons.telephone), SUBSTRING(code_postal, 1, 2) FROM dons INNER JOIN users u on u.telephone = dons.telephone GROUP BY SUBSTRING(code_postal, 1, 2) ORDER BY COUNT(dons.telephone) DESC;   
+        SELECT COUNT(dons.telephone) as tel, SUBSTRING(code_postal, 1, 2) as cp FROM dons INNER JOIN users u on u.telephone = dons.telephone 
+        GROUP BY cp ORDER BY tel DESC LIMIT 10;   
         ');
 
+        //Requète pour la somme des autre departement en dehors des 10 premier:
+        $somme_dep_restant = mysqli_query($dbc, '
+        SELECT SUM(dep) as total_dep FROM (
+        SELECT COUNT(telephone) as dep FROM users GROUP BY SUBSTRING(code_postal, 1, 2) ORDER BY dep DESC LIMIT 90 OFFSET 10
+        ) as total;
+        ');
+
+        //Insertion resultats dans le tableau
         foreach ($departement->fetch_all() as $row){
             $res = (object)array("montant_total" => $row[0], "departement"=>$row[1]);
             $datas [] = $res;
         }
+        $other_dep = (object)array("montant_total"=>$somme_dep_restant->fetch_array()['total_dep'], "departement"=>"Autres");
+        $datas [] = $other_dep;
 
         return $this->json($datas, status: 200);
     }

d17be448a06f4d9911d10d984926f2d8e77a328d
ClaireK01 <ckalsto01@gmail.com>
2022-11-02 16:23:07 +0100

    optimisation requete api barchart

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -23,28 +23,21 @@ class DashboardController extends AbstractController
     #[Route('/api_barchart', name: 'app_api_barchart',  options:['expose'=>true])]
     public function api_barChart() : JsonResponse {
 
-
-        $db = new \PDO('mysql:host=localhost;dbname=high_connexion', "root", "");
-        $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        $db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
-
         $dbc = mysqli_connect('localhost', 'root', "", "high_connexion");
-        $dons1 = mysqli_query($dbc, 'SELECT COUNT(*) as number FROM dons WHERE montant = 1' )
-                ->fetch_array()['number'];
-        $dons2 = mysqli_query($dbc, 'SELECT COUNT(*) as number FROM dons WHERE montant = 2' )
-            ->fetch_array()['number'];
-        $dons3 = mysqli_query($dbc, 'SELECT COUNT(*) as number FROM dons WHERE montant = 3' )
-            ->fetch_array()['number'];
-        $dons4 = mysqli_query($dbc, 'SELECT COUNT(*) as number FROM dons WHERE montant = 4' )
-            ->fetch_array()['number'];
-        $dons5 = mysqli_query($dbc, 'SELECT COUNT(*) as number FROM dons WHERE montant = 5' )
-            ->fetch_array()['number'];
-        $dons6 = mysqli_query($dbc, 'SELECT COUNT(*) as number FROM dons WHERE montant = 6' )
-            ->fetch_array()['number'];
-        $dons6Plus = mysqli_query($dbc, 'SELECT COUNT(*) as number FROM dons WHERE montant > 6' )
-            ->fetch_array()['number'];
+        $datas = [];
+        for ($i = 1; $i <= 7; $i ++){
+            if($i < 6 ){
+                $don = mysqli_query($dbc, 'SELECT COUNT(*) as number FROM dons WHERE montant = '. $i .';')
+                    ->fetch_array()['number'];
+                $datas [] = $don;
+            }else{
+                $don = mysqli_query($dbc, 'SELECT COUNT(*) as number FROM dons WHERE montant > 6 ;')
+                    ->fetch_array()['number'];
+                $datas [] = $don;
+            }
+        }
 
-       return $this->json([$dons1, $dons2, $dons3, $dons4, $dons5, $dons6, $dons6Plus], status: 200);
+       return $this->json($datas, status: 200);
     }
 
     #[Route('/api_piechart', name: 'app_api_piechart', options:['expose'=>true])]
